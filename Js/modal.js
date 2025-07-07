5bd1f1bb2d0c7fa48481313793ce60ae2a3182f6
SerhiiKharyponcuk <hariponcuksergej@gmail.com>
2025-07-07 23:22:46 +0200

    hw-26.3

--- Js/modal.js
+++ Js/modal.js
@@ -1,13 +1,15 @@
-//! Бєк-дроп з модальним вікном
-const openModalBtn = document.querySelector("[data-modal-open]");
-const modal = document.querySelector("[data-modal]");
-const closeModalBtn = document.querySelector("[data-modal-close]");
+document.addEventListener("DOMContentLoaded", () => {
+  const openModalBtn = document.querySelector("[data-modal-open]");
+  const modal = document.querySelector("[data-modal]");
+  const closeModalBtn = document.querySelector("[data-modal-close]");
 
-
-openModalBtn.addEventListener("click", toggleModal);
-closeModalBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
+  function toggleModal() {
     modal.classList.toggle("is-hidden");
     document.body.classList.toggle("no-scroll");
-};
+  }
+
+  if (openModalBtn && closeModalBtn) {
+    openModalBtn.addEventListener("click", toggleModal);
+    closeModalBtn.addEventListener("click", toggleModal);
+  }
+});
